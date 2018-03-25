a976e7e2b3fe8144b95e83b24b82b0244db04b85
etcdotx <36808121+etcdotx@users.noreply.github.com>
2018-03-25 14:00:30 +0700

    Game Files
    w

---
+++ SoftwareEngineer/Assets/TouchManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+	private float drag_distance;
+	private Vector2 fp;
+	private Vector2 lp;
+
+	// Use this for initialization
+	void Start () {
+		drag_distance = Screen.width * 15 / 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount == 1) {
+
+		}
+	}
+}
